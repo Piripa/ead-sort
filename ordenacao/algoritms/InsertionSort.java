0966caaeb4a6e285d5da3e574924abe15766baaa
Vinicius Alessandro <103452537+Piripa@users.noreply.github.com>
2023-03-07 20:52:07 +0000

    Insertio Adicionado

--- ordenacao/algoritms/InsertionSort.java
+++ ordenacao/algoritms/InsertionSort.java
@@ -6,8 +6,16 @@ public class InsertionSort implements Sorter{
 
     @Override
     public int[] sort(int[] elements) {
+        for (int i = 0; i < elements.length; i++){
+            int key= i+1;
+            for (int j = i; j < elements.length; j++) {
+                if(elements[j]<key){
+                    elements[j] = key;
+                }
+                key = elements[j];
+            }
+        }
         int [] sorted = elements.clone();
-        //TODO Implementar
         return sorted;
     }
 
--- ordenacao/algoritms/SelectionSort.java
+++ ordenacao/algoritms/SelectionSort.java
@@ -6,11 +6,21 @@ public class SelectionSort implements Sorter{
 
     @Override
     public int[] sort(int[] elements) {
+        for (int i = 0; i < elements.length; i++) {
+            int key = i;
+            for (int j = i+1; j < elements.length; j++) {
+                if(key>elements[j]){
+                    int save = key;
+                    key = elements[j];
+                    elements[j] = save;
+                }
+            }
+            
+        }
         int [] sorted = elements.clone();
-        //TODO Implementar
         return sorted;
     }
-
+    
     @Override
     public String getName() {
         return "SelectionSort";
