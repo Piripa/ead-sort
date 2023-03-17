342594d3e2a4196e19c9c90e1924cf1fdbae4cc3
Vinicius <viniaoliveira22@gmail.com>
2023-03-17 20:53:16 -0300

    Algoritmos de Ordenação completos

--- ordenacao/algoritms/MergeSort.java
+++ ordenacao/algoritms/MergeSort.java
@@ -6,9 +6,41 @@ public class MergeSort implements Sorter {
 
     @Override
     public int[] sort(int[] elements) {
+        merge(elements, elements, 0, elements.length-1);
         int [] sorted = elements.clone();
-        
         return sorted;
+    }
+    private void merge(int[] elements,int[] auxiliar, int inicio, int fim)
+    {  
+        if(inicio<fim){
+            int meio = (inicio+fim)/2;
+            merge(elements, auxiliar, inicio, meio);
+            merge(elements, auxiliar, meio+1, fim);
+            juntar(elements, auxiliar, inicio, fim, meio);
+
+        }
+    }
+    private void juntar(int[]elements, int[] auxiliar, int inicio, int fim, int meio){
+        int esq = inicio;
+        int dir = meio+1;
+        for (int i = inicio; i <=fim; i++) {
+            auxiliar[i] = elements[i];
+        }
+        for (int i = inicio; i < fim; i++) {
+            if(esq > meio){
+                elements[i] = auxiliar[dir++];
+            }
+            else if(dir>fim){
+                elements[i] = auxiliar[esq++];
+            }
+            else if(auxiliar[esq]< auxiliar[dir]){
+                elements[i] = auxiliar[esq++];
+            }
+            else{
+                elements[i] = auxiliar[dir++];
+            }
+            
+        }
     }
 
     @Override
--- ordenacao/algoritms/QuickSort.java
+++ ordenacao/algoritms/QuickSort.java
@@ -6,13 +6,40 @@ public class QuickSort implements Sorter {
 
     @Override
     public int[] sort(int[] elements) {
+        quickSort(elements, 0, elements.length-1);
         int [] sorted = elements.clone();
         return sorted;
-    }
+        }
+    private void quickSort(int[] elements, int esq, int dir){
+        if(esq<dir){
+            int pivo = logica(elements, esq, dir);
+            quickSort(elements, esq, pivo-1);
+            quickSort(elements, pivo+1, dir);
+        }
 
-    @Override
-    public String getName() {
-        return "QuickSort";
     }
+    private int logica(int[] elements, int esq, int dir){
+        int pivo = elements[dir];
+        int index = esq-1;
+        for (int i = esq; i< dir; i++) {
+                if(elements[i]<=pivo){
+                    index++;
+                    int save = elements[index];
+                    elements[index] = elements[i];
+                    elements[i] = save;
+                }
+        }
+        int aux = elements[index+1];
+        elements[index+1]=elements[dir];
+        elements[dir] = aux;
 
-}
+        return index+1;
+    }
+        
+        @Override
+        public String getName() {
+            return "QuickSort";
+        }
+    }
+        
+ 
